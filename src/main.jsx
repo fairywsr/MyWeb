087b455fa4d8ccf51524661b4515b874b812dc3c
Faria Waseer <fariawaseer@gmail>
2024-12-26 23:42:17 +0500

    pages added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Layout from './Layout';
+import Home from './Components/Home/Home';
+import Team from './Components/Team/Team';
+import Projects from "./Components/Projects/Projects";
+import Services from './Components/Services/Services';
+import AboutUs from './Components/AboutUs/AboutUs';
+import ContactUs from './Components/ContactUs/ContactUs';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: 'home',
+        element: <Home />
+      },
+      {
+        path: 'team',
+        element: <Team />
+      },
+      {
+        path: 'projects',
+        element: <Projects />
+      },
+      {
+        path: 'services',
+        element: <Services />
+      },
+      {
+        path: 'about-us',
+        element: <AboutUs />
+      }
+      ,
+      {
+        path: 'contact-us',
+        element: <ContactUs />
+      }
+    ]
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+   <RouterProvider router ={router} ></RouterProvider>
+  </StrictMode>
 )
